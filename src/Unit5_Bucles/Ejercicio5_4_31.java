6502e11d45baa0ee31154056cf0ea594238c66bc
Saul Echeverri <secheverri@cidenet.com.co>
2023-08-23 11:44:31 -0500

    Ejercicio5_4_30 resuelto

---
+++ src/Unit5_Bucles/Ejercicio5_4_31.java
@@ -0,0 +1,17 @@
+package Unit5_Bucles;
+
+import javax.swing.*;
+
+/* @author: Saulolo */
+public class Ejercicio5_4_31 {
+
+    /* Realiza un programa que pinte la letra L por pantalla hecha con asteriscos. El programa pedirá la
+    altura. El palo horizontal de la L tendrá una longitud de la mitad (división entera entre 2) de la
+    altura más uno..*/
+
+    public static void main(String[] args) {
+        System.out.println("PINTANDO LA LETRA L");
+        System.out.println("-------------------");
+
+    }
+}
