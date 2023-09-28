01ab9fab9a9a02551f14ad1c012a4562822622a2
Saul Echeverri <secheverri@cidenet.com.co>
2023-09-28 09:55:04 -0500

    :memo: Finalizado ejercicio 32 Unidad 6 Números aleatrios

---
+++ src/Uni6_NumerosAleatorios/Ejercicio6_3_31.java
@@ -0,0 +1,90 @@
+package Uni6_NumerosAleatorios;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+
+/* todo: @author: Saulolo */
+
+/* Realiza el juego del “Craps”. Las reglas son las siguientes: Al comenzar la partida, el jugador introduce la
+cantidad de dinero que quiere apostar. Se muestra la tirada aleatoria de dos dados. Si entre los dos dados suman
+7 u 11, el jugador gana la misma cantidad que apostó y termina la partida. Por ej. si apostó 1000 €, gana otros
+1000 € y acaba con 2000 €. Si entre los dos dados suman 2, 3 o 12, el jugador pierde el total de su dinero y
+termina la partida. Si no se da ninguno de los casos anteriores, es decir si sale 4, 5, 6, 8, 9 o 10, el juego
+entra en una segunda etapa. En esta etapa, el jugador buscará volver a obtener ese número en los dados. Si
+consigue repetir ese número, gana. Si sale un 7, pierde. Si sale otro número, tiene que seguir tirando.
+
+*/
+
+
+public class Ejercicio6_3_31 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String into = ("JUGANDO CRAPS");
+        JOptionPane.showMessageDialog(null, into);
+
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ ⚀ C ⚁ R ⚂ A ⚃ P ⚄ S ⚅  ┃");
+        System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("¿Cuánto dinero quiere apostar? ");
+        int dinero = Integer.parseInt(s.nextLine());
+
+        boolean juegoTerminado = false;
+
+        int dado1 = (int) (Math.random() * 6 + 1);
+        int dado2 = (int) (Math.random() * 6 + 1);
+        int suma = dado1 + dado2;
+
+        System.out.println("Dado 1: " + dado1);
+        System.out.println("Dado 2: " + dado2);
+        System.out.println("Suma: " + suma);
+
+        switch (suma) {
+            case 7:
+            case 11:
+                System.out.println("¡Enhorabuena! ¡Ha ganado otros " + dinero + " €!");
+                System.out.println("¡Ahora tiene " + dinero * 2 + " €!");
+                break;
+            case 2:
+            case 3:
+            case 12:
+                System.out.println("Lo siento, ha perdido todo su dinero 😢");
+                break;
+            default:
+                System.out.print("Tiene que seguir tirando, debe conseguir el ");
+                System.out.println(suma + " para ganar.");
+                System.out.println("Si obtiene un 7, perderá la partida.");
+                System.out.println("Pulse INTRO para tirar los dados.");
+                s.nextLine();
+
+                boolean partidaTerminada = false;
+
+                do {
+                    dado1 = (int) (Math.random() * 6 + 1);
+                    dado2 = (int) (Math.random() * 6 + 1);
+
+                    System.out.println("Dado 1: " + dado1);
+                    System.out.println("Dado 2: " + dado2);
+                    System.out.println("Suma: " + (dado1 + dado2));
+
+                    if ((dado1 + dado2) == suma) {
+                        System.out.println("¡Enhorabuena! ¡Ha ganado otros " + dinero + " €!");
+                        System.out.println("¡Ahora tiene " + dinero * 2 + " €!");
+                        partidaTerminada = true;
+                    } else if ((dado1 + dado2) == 7) {
+                        System.out.println("Lo siento, ha perdido todo su dinero 😢");
+                        partidaTerminada = true;
+                    } else {
+                        System.out.println("Continúe jugando.");
+                        System.out.println("Pulse INTRO para tirar los dados.");
+                        s.nextLine();
+                    }
+                } while (!partidaTerminada);
+        }
+    }
+    
+}
+
---
+++ src/Uni6_NumerosAleatorios/Ejercicio6_3_32.java
@@ -0,0 +1,75 @@
+package Uni6_NumerosAleatorios;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+
+/* todo: @author: Saulolo */
+
+/* Realiza un programa que pinte un sendero aleatorio. Los bordes se pintan con el carácter “|”. La anchura del
+sendero siempre es la misma, los dos caracteres del borde más cuatro caracteres en medio, en total 6 caracteres
+(incluyendo espacios). A cada metro, el sendero puede continuar recto, girar un carácter a la izquierda o girar
+un carácter a la derecha, por supuesto de forma aleatoria. Por cada metro de sendero - representado por una línea -
+puede que haya un obstáculo o puede que no, la probabilidad es del 50%. La posición del obstáculo es aleatoria
+dentro de la línea. En caso de existir un obstáculo en un metro de sendero (en una línea), puede ser una planta
+(carácter *) o una piedra (carácter O), la probabilidad de que salga uno u otro es la misma. Recuerda que nunca
+habrá más de un obstáculo por metro de sendero, habrá uno o ninguno.
+
+Ejemplo 1:
+Introduzca la longitud del sendero en metros: 7
+| * |
+ | O|
+ | |
+| * |
+ |* |
+  | O |
+   | |
+
+*/
+
+public class Ejercicio6_3_32 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String into = ("PINTANDO UN SENDERO ALEATORIO");
+        JOptionPane.showMessageDialog(null, into);
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Introduzca la longitud del sendero en metros: ");
+        int longitudSendero = Integer.parseInt(s.nextLine());
+
+        int espaciosPorDelante = 6;
+
+        for (int i = 0; i < longitudSendero; i++) {
+            // Espacios por delante
+            for (int j = 0; j < espaciosPorDelante; j++) {
+                System.out.print(' ');
+            }
+
+            // Borde izquierdo del sendero
+            System.out.print('|');
+
+            // Parte interior del sendero
+            int posicionObstaculo = -1;
+            char obstaculo = '*'; // planta por defecto
+
+            if ((int) (Math.random() * 2) == 0) { // hay obstáculo
+                posicionObstaculo = (int) (Math.random() * 4);
+                if ((int) (Math.random() * 2) == 0) { // piedra
+                    obstaculo = 'O';
+                }
+            }
+
+            for (int j = 0; j < 4; j++) {
+                System.out.print(j == posicionObstaculo ? obstaculo : ' ');
+            }
+
+            // Borde derecho del sendero
+            System.out.println('|');
+
+            espaciosPorDelante += (int) (Math.random() * 3) - 1;
+        }
+    }
+}
+
