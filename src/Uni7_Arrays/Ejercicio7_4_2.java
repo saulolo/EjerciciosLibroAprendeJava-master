06ded3a6c71837ba4838ea0ad3d859981b2c1659
Saul Echeverri <secheverri@cidenet.com.co>
2023-10-05 05:08:48 -0500

    :memo:  Finalizado Ejercicio7_4_3

---
+++ src/Uni7_Arrays/Ejercicio7_4_2.java
@@ -0,0 +1,37 @@
+package Uni7_Arrays;
+
+import javax.swing.*;
+
+/**
+ * Define un array de 10 caracteres con nombre simbolo y asigna valores a los elementos según la tabla que se
+ * muestra a continuación. Muestra el contenido de todos los elementos del array. ¿Qué sucede con los valores
+ * de los elementos que no han sido inicializados?
+ * @author Saulolo
+ */
+public class Ejercicio7_4_2 {
+
+    public static void main(String[] args)  {
+
+        String into = ("MOSTRANDO UN ARRAY DE CARACTERES");
+        JOptionPane.showMessageDialog(null, into);
+
+        String[] simbolo = new String[10];
+        simbolo[0] = "a";
+        simbolo[1] = "X";
+        simbolo[4] = "@";
+        simbolo[6] = "''";
+        simbolo[7] = "+";
+        simbolo[8] = "Q";
+
+        for (String caracter : simbolo) {
+            System.out.println(caracter);
+        }
+
+        /**
+         * Por defecto, los elementos que no se inicializaron, se inicializan en null.
+         */
+
+
+    }
+}
+
---
+++ src/Uni7_Arrays/Ejercicio7_4_3.java
@@ -0,0 +1,21 @@
+package Uni7_Arrays;
+
+import javax.swing.*;
+
+/**
+ * Escribe un programa que lea 10 números por teclado y que luego los muestre en orden inverso, es decir, el
+ * primero que se introduce es el último en mostrarse y viceversa.
+ * @author Saulolo
+ */
+public class Ejercicio7_4_3 {
+
+    public static void main(String[] args)  {
+
+        String into = ("MOSTRANDO UN ARRAY INVERSO");
+        JOptionPane.showMessageDialog(null, into);
+
+
+
+    }
+}
+
