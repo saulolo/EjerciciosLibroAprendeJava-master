5e5714bad32955c16d194996b824612c640a44c6
Saul Echeverri <secheverri@cidenet.com.co>
2023-10-06 05:55:47 -0500

    :memo:  Finalizado Ejercicio7_4_4

--- src/Uni7_Arrays/Ejercicio7_4_3.java
+++ src/Uni7_Arrays/Ejercicio7_4_3.java
@@ -1,6 +1,7 @@
 package Uni7_Arrays;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 /**
  * Escribe un programa que lea 10 números por teclado y que luego los muestre en orden inverso, es decir, el
@@ -11,11 +12,27 @@ public class Ejercicio7_4_3 {
 
     public static void main(String[] args)  {
 
+        Scanner input = new  Scanner(System.in);
+
         String into = ("MOSTRANDO UN ARRAY INVERSO");
         JOptionPane.showMessageDialog(null, into);
 
+        int[] numeros = new int[10];
 
+        System.out.println("Ingresa 10 números, por favor: ");
 
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Número " + (i + 1) + ":"); //Asi pido número por número.
+            numeros[i] = input.nextInt();
+        }
+
+        System.out.println("Los números ingresados en orden inverso son: ");
+
+        for (int j = 9; j >= 0; j--) {
+            System.out.println(numeros[j] + " ");
+
+        }
     }
 }
 
---
+++ src/Uni7_Arrays/Ejercicio7_4_4.java
@@ -0,0 +1,24 @@
+package Uni7_Arrays;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+/**
+ * Define tres arrays de 20 números enteros cada una, con nombres numero, cuadrado y cubo. Carga el array numero
+ * con valores aleatorios entre 0 y 100. En el array cuadrado se deben almacenar los cuadrados de los valores que
+ * hay en el array numero. En el array cubo se deben almacenar los cubos de los valores que hay en numero. A
+ * continuación, muestra el contenido de los tres arrays dispuesto en tres columnas.
+ * @author Saulolo
+ */
+public class Ejercicio7_4_4 {
+
+    public static void main(String[] args)  {
+
+
+        String into = ("MOSTRANDO TRES ARRAYS EN TRES COLUMNAS");
+        JOptionPane.showMessageDialog(null, into);
+
+
+    }
+}
+
