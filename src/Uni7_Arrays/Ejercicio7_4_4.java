f6cbd13da78ef672f1194bbfa5a00c26d4484244
Saulolo <saulolo@gmail.com>
2023-10-09 05:23:27 -0500

    :memo:  Finalizado Ejercicio7_4_7

--- src/Uni7_Arrays/Ejercicio7_4_4.java
+++ src/Uni7_Arrays/Ejercicio7_4_4.java
@@ -18,7 +18,28 @@ public class Ejercicio7_4_4 {
         String into = ("MOSTRANDO TRES ARRAYS EN TRES COLUMNAS");
         JOptionPane.showMessageDialog(null, into);
 
+        int[] numero = new int[20];
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
+
+        int i;
+
+        for (i = 0; i < 20; i++) {
+            numero[i] = (int)(Math.random()*101);
+            cuadrado[i] = numero[i] * numero[i];
+            cubo[i] = cuadrado[i] * numero[i];
+        }
+
+        System.out.println("A continuación se muestran en tres columnas, un número aleatorio entre " +
+                "0 y 100, su cuadrado y su cubo: ");
+
+        System.out.println("  n  │   n²  │    n³\n─────┼───────┼─────────");
+        for (i = 0; i < 20; i++) {
+            System.out.printf("%4d │ %5d │%8d\n", numero[i], cuadrado[i], cubo[i]);
+        }
 
     }
+
 }
+
 
---
+++ src/Uni7_Arrays/Ejercicio7_4_7.java
@@ -0,0 +1,25 @@
+package Uni7_Arrays;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+/**
+ * Escribe un programa que genere 100 números aleatorios del 0 al 20 y que los muestre por pantalla
+ * separados por espacios. El programa pedirá entonces por teclado dos valores y a continuación
+ * cambiará todas las ocurrencias del primer valor por el segundo en la lista generada anteriormente.
+ * Los números que se han cambiado deben aparecer entrecomillados.
+ * @author Saulolo
+ */
+public class Ejercicio7_4_7 {
+
+    public static void main(String[] args)  {
+
+        Scanner input = new Scanner(System.in);
+
+        String into = ("CAMBIANDO LAS OCURRENCIAS DE LOS NÚMEROS");
+        JOptionPane.showMessageDialog(null, into);
+
+
+        
+    }
+}
